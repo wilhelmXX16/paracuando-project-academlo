9ab4123d3af44261c3ce40cb9f38282fda42d858
wilhelmXX16 <wjc1619t@gmail.com>
2023-03-23 21:35:23 -0500

    feat: create view create publication

--- pages/publications/index.tsx
+++ pages/publications/index.tsx
@@ -4,8 +4,8 @@ import { NextPageWithLayout } from '../page';
 
 const CreatePublication: NextPageWithLayout = () => {
   return (
-    <div className="grid md:grid-flow-col-2 h-screen">
-      <div className="bg-app-blue md:flex items-center justify-center w-[255px] p-5">
+    <div className="flex flex-row flex-wrap h-screen border-solid border-2 border-red-500 gap-4">
+      <div className="bg-app-blue md:flex items-center justify-center w-[255px] p-5 border-solid border-2 border-yellow-500">
         <Link href={'/'}>
           <div className="">
             <Logo variant="white" />
@@ -18,9 +18,51 @@ const CreatePublication: NextPageWithLayout = () => {
           </div>
         </Link>
       </div>
-      <article className="w-full text-left flex flex-col gap-8">
-        <h2 className="text-app-blue">Back</h2>
-      </article>
+      <div className=" w-full max-w-lg">
+        <Link href={'/'}>
+          <h1 className="text-app-blue mt-8">Back</h1>
+        </Link>
+        <div className="border-solid border-1 bg-app-grayLight h-1 w-[612px] rounded-md">
+          <div className=" bg-app-blue h-0 w-[361px]"></div>
+        </div>
+        <h1>Publicación</h1>
+        <samp>Información básica</samp>
+        <form>
+          <label className="flex flex-col gap-1">
+            <span className="font-semibold">Titulo de publicación</span>
+            <input className="p-4 border border-app-grayDark" type="text" />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span className="font-semibold">Titulo de publicación</span>
+            <input className="p-4 border border-app-grayDark" type="text" />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span className="font-semibold">Tipo</span>
+            <select name="tipo">
+              <option>---</option>
+              <option>---</option>
+              <option>---</option>
+            </select>
+          </label>
+          <label className="flex flex-col gap-1">
+            <span className="font-semibold">Categoria</span>
+            <select name="tipo">
+              <option>---</option>
+              <option>---</option>
+              <option>---</option>
+            </select>
+          </label>
+          <label className="flex flex-col gap-1">
+            <span className="font-semibold">¿Por qué lo recomiendas?</span>
+            <input className="p-4 border border-app-grayDark" type="stext" />
+          </label>
+          <label className="flex flex-col gap-1">
+            <span className="font-semibold">Link de referencia</span>
+            <input className="p-4 border border-app-grayDark" type="stext" />
+          </label>
+        </form>
+        <button>Siguiente</button>
+      </div>
     </div>
   );
 };
