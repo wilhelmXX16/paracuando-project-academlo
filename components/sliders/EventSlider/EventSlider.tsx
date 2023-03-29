054665921f238a413f6367be7833460115b75093
wilhelmXX16 <wjc1619t@gmail.com>
2023-03-29 15:23:17 -0500

    feat: consume the endpoint and display the cards in the category views

--- components/sliders/EventSlider/EventSlider.tsx
+++ components/sliders/EventSlider/EventSlider.tsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 interface IEventSlider {
   title?: string;
   subtitle?: string;
-  events: IEvents[];
+  events: Publication[];
 }
 
 export const EventSlider: FC<IEventSlider> = ({ title, subtitle, events }) => {
@@ -44,61 +44,67 @@ export const EventSlider: FC<IEventSlider> = ({ title, subtitle, events }) => {
             },
           }}
         >
-          {events?.map((event, index) => (
-            <SwiperSlide key={index}>
-              {/* ------------Card Populares-----------------*/}
-              <div>
-                <div className=" border border-app-grayLight shadow-lg shadow-app-gray/50 rounded-[20px] my-2 mx-1 w-[300px] h-[454px]">
-                  <div id="img" className="h-[239px]">
-                    <img
-                      src={event.image}
-                      alt="mock"
-                      className="rounded-t-[20px] h-[239px]"
-                    />
-                  </div>
-                  <div
-                    id="inf"
-                    className=" app-text-1 px-6 py-4 flex-col gap-4  relative"
-                  >
-                    <div id="informacion" className=" h-[130px]">
-                      <a href="#" className="app-title-3 ">
-                        {event.title}
-                      </a>
-                      <div className="relative max-h-[76px] min-h-[60px] overflow-hidden ">
-                        <p className="w-full h-full  text-app-grayDark pt-2 ">
-                          {event.short_description}
-                        </p>
-                      </div>
-                    </div>
-                    <div id="link" className="flex-col flex gap-3  ">
-                      <a
-                        href="#"
-                        className="text-app-subtitle-2 text-app-blue font-semibold"
-                      >
-                        {event.url}
-                      </a>
-                      <div className="flex gap-2 items-center">
-                        {/*<img src="/public/img/Vector.svg" alt="">*/}
-                        <img src="Vector.svg" alt="mock" />
-                        <span>{event.votes} votos</span>
-                      </div>
+          {events?.map((event, index) => {
+            return (
+              <SwiperSlide key={index}>
+                {/* ------------Card Populares-----------------*/}
+                <div>
+                  <div className=" border border-app-grayLight shadow-lg shadow-app-gray/50 rounded-[20px] my-2 mx-1 w-[300px] h-[454px]">
+                    <div id="img" className="h-[239px]">
+                      <img
+                        src={
+                          event.images
+                            ? event.images[0].image_url
+                            : 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled.png'
+                        }
+                        alt="mock"
+                        className="rounded-t-[20px] h-[239px]"
+                      />
                     </div>
                     <div
-                      id="corazon"
-                      className="relative top-[-221px]  left-[215px]"
+                      id="inf"
+                      className=" app-text-1 px-6 py-4 flex-col gap-4  relative"
                     >
-                      {/*<img src="/public/img/like.svg" alt="">*/}
-                      <img
-                        src="like.svg"
-                        alt="mock"
-                        className="cursor-pointer "
-                      />
+                      <div id="informacion" className=" h-[130px]">
+                        <a href="#" className="app-title-3 ">
+                          {event.title}
+                        </a>
+                        <div className="relative max-h-[76px] min-h-[60px] overflow-hidden ">
+                          <p className="w-full h-full  text-app-grayDark pt-2 ">
+                            {event.description}
+                          </p>
+                        </div>
+                      </div>
+                      <div id="link" className="flex-col flex gap-3  ">
+                        <a
+                          href="#"
+                          className="text-app-subtitle-2 text-app-blue font-semibold"
+                        >
+                          {event.reference_link}
+                        </a>
+                        <div className="flex gap-2 items-center">
+                          {/*<img src="/public/img/Vector.svg" alt="">*/}
+                          <img src="Vector.svg" alt="mock" />
+                          <span>{event.votes_count}</span>
+                        </div>
+                      </div>
+                      <div
+                        id="corazon"
+                        className="relative top-[-221px]  left-[215px]"
+                      >
+                        {/*<img src="/public/img/like.svg" alt="">*/}
+                        <img
+                          src="like.svg"
+                          alt="mock"
+                          className="cursor-pointer "
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
+              </SwiperSlide>
+            );
+          })}
           <div className="hidden sm:flex items-center absolute top-0 bottom-0 -right-20 left-auto cursor-pointer">
             <SlideNextButton />
           </div>
@@ -113,7 +119,7 @@ export const EventSlider: FC<IEventSlider> = ({ title, subtitle, events }) => {
 
 // some-inner-component.jsx
 import { useSwiper } from 'swiper/react';
-import { IEvents } from '../../../lib/interfaces/event.interface';
+import { Publication } from '../../../lib/interfaces/publications.interface';
 
 interface ISlideNextButton {
   className?: string;
